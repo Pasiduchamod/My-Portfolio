aac118e69e3d85e049bf47c3bc3926fbbca3fd6c
Pasiduchamod <jayaweera.pasindu123@gmail.com>
2025-02-03 01:20:46 +0530

    Added My portfolio highlights

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import About from "./components/About";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import Skill from "./components/Skill";
+import Work from "./components/Work";
 
 const App = () =>{
     return (
@@ -9,6 +11,8 @@ const App = () =>{
         <main>
         <Hero/>
         <About/>
+        <Skill/>
+        <Work/>
         </main>
     </>
     )
